8d88d35b0aa088e54cc62a57b7cbfe85a10fae83
keneth-00 <gryvanslvn@gmail.com>
2025-12-10 12:40:01 +0800

    Update source code (with bonus).cpp

--- source code (with bonus).cpp
+++ source code (with bonus).cpp
@@ -4,161 +4,161 @@
 using namespace std;
 
 //Function to compute salary
- 
+
 double computeSalary(double hoursWorked, double hourlyPay, double overTime, double bonus)
 {
     //Conditional statement to separate overtime pay from regular pay
 
     double regularHours;
     double grossPay;
-    
+
     if (hoursWorked > 160)
     {
-    	regularHours = 160;
+            regularHours = 160;
     }
-    	else
-    	{
-    		regularHours = hoursWorked;
-    	}
-    	
+            else
+            {
+                    regularHours = hoursWorked;
+            }
+
     grossPay = (regularHours * hourlyPay) + overTime + bonus;
-    
+
     return grossPay;
 }
- 
+
 //Function to display payslip summary
 
 void displaySummary(string idNum, string eName, double hoursWorked, double hourlyPay, double grossPay, double overTime, double bonus, double cashLoan, double sss, double sssL, double pagibig, double pagibigL, double philH, double otherDeduct, double totalDeduct, double netSalary)
  {
- 	cout<<endl<<endl<<"========SALARY PAYSLIP========"<<endl<<endl;
- 	cout<<"Employee ID Number: "<<idNum<<endl;
- 	cout<<"Employee Name: "<<eName<<endl<<endl;
- 	cout<<"Hours Worked: "<<hoursWorked<<endl;
- 	cout<<"Hourly Rate: "<<hourlyPay<<endl<<endl;
- 	cout<<"Gross Salary: "<<grossPay<<endl;
- 	cout<<endl<<"ADDITIONAL (already added in Gross Salary)"<<endl;
- 	cout<<"Overtime Pay: "<<overTime<<endl;
- 	cout<<"Bonus: "<<bonus<<endl<<endl;
- 	
- 	cout<<"DEDUCTIONS"<<endl;
- 	cout<<"Cash Advance: "<<cashLoan<<endl;
- 	cout<<"SSS Contribution: "<<sss<<endl;
- 	cout<<"SSS Loan: "<<sssL<<endl;
- 	cout<<"Pag-Ibig Contribution: "<<pagibig<<endl;
- 	cout<<"Pag-Ibig MPL: "<<pagibigL<<endl;
- 	cout<<"PhilHealth: "<<philH<<endl;
- 	cout<<"Others:  "<<otherDeduct<<endl<<endl;
- 	
- 	cout<<"Total Deductions: "<<totalDeduct<<endl<<endl;
- 	
- 	cout<<"Net Pay: "<<netSalary<<endl<<endl;
- 	cout<<"=============================="<<endl<<endl;
+         cout<<endl<<endl<<"========SALARY PAYSLIP========"<<endl<<endl;
+         cout<<"Employee ID Number: "<<idNum<<endl;
+         cout<<"Employee Name: "<<eName<<endl<<endl;
+         cout<<"Hours Worked: "<<hoursWorked<<endl;
+         cout<<"Hourly Rate: "<<hourlyPay<<endl<<endl;
+         cout<<"Gross Salary: "<<grossPay<<endl;
+         cout<<endl<<"ADDITIONAL (already added in Gross Salary)"<<endl;
+         cout<<"Overtime Pay: "<<overTime<<endl;
+         cout<<"Bonus: "<<bonus<<endl<<endl;
+
+         cout<<"DEDUCTIONS"<<endl;
+         cout<<"Cash Advance: "<<cashLoan<<endl;
+         cout<<"SSS Contribution: "<<sss<<endl;
+         cout<<"SSS Loan: "<<sssL<<endl;
+         cout<<"Pag-Ibig Contribution: "<<pagibig<<endl;
+         cout<<"Pag-Ibig MPL: "<<pagibigL<<endl;
+         cout<<"PhilHealth: "<<philH<<endl;
+         cout<<"Others:  "<<otherDeduct<<endl<<endl;
+
+         cout<<"Total Deductions: "<<totalDeduct<<endl<<endl;
+
+         cout<<"Net Pay: "<<netSalary<<endl<<endl;
+         cout<<"=============================="<<endl<<endl;
  }
 
 //Main source code
 
 int main()
 {
-	int n, category;
-	string id, name;
-		double hours, hourlyRate, grossSalary, otPay, bonusPay, netPay;
-		double cashAdvance, sssContri, pagibigContri, philHealth, sssLoan, pagibigLoan, others, totalDeduction;
-	
-//Ask for number of employees to process
+        int n, category;
+        string id, name;
+                double hours, hourlyRate, grossSalary, otPay, bonusPay, netPay;
+                double cashAdvance, sssContri, pagibigContri, philHealth, sssLoan, pagibigLoan, others, totalDeduction;
 
-	cout<<"SALARY PAYSLIP"<<endl<<endl;
-	cout<<"Enter number of employees to process: ";
-	cin>>n;
+		//Ask for number of employees to process
 
-//Loop to process multiple employees
+        cout<<"SALARY PAYSLIP"<<endl<<endl;
+        cout<<"Enter number of employees to process: ";
+        cin>>n;
 
-	for (int l = 0; l < n; l++)
-	{
+		//Loop to process multiple employees
 
-//Ask for employee details
+        for (int l = 0; l < n; l++)
+        {
 
-		cout<<endl<<"ENTER EMPLOYEE "<<l + 1<<" DETAILS: "<<endl;
-		cout<<"ID Number: ";
-		cin>>id;
-		cout<<"Employee Name: ";
-		cin.ignore();
-		getline(cin, name);
-	
-//Ask for hours and rate
+				//Ask for employee details
 
-		cout<<endl<<"Enter hours worked: ";
-		cin>>hours;
-		cout<<"Enter hourly rate: ";
-		cin>>hourlyRate;
-		
-//Conditional statement to compute overtime pay
+                cout<<endl<<"ENTER EMPLOYEE "<<l + 1<<" DETAILS: "<<endl;
+                cout<<"ID Number: ";
+                cin>>id;
+                cout<<"Employee Name: ";
+                cin.ignore();
+                getline(cin, name);
 
-		if (hours > 160)
- 	{
- 		otPay = (hours - 160) * (hourlyRate * 1.25);
- 	}
- 		else 
- 		{
- 			otPay = 0;
- 		}	
- 		
-//Select bonus category
+				//Ask for hours and rate
 
- 		cout<<endl<<"Select a bonus category (1 - Standard, 2 - Premium, 3 - None): ";
- 		cin>>category;
- 		
- 		switch (category)	
- 		{
-	 		case 1:
- 				bonusPay = 500;
- 				break;
- 			case 2:
- 				bonusPay = 1000;
- 				break;
- 			case 3:
- 				bonusPay = 0;
- 				break;
- 			default:
- 				cout<<"Invalid category. No bonus applied.";
- 				bonusPay = 0;
- 				break;
- 		}
+                cout<<endl<<"Enter hours worked: ";
+                cin>>hours;
+                cout<<"Enter hourly rate: ";
+                cin>>hourlyRate;
 
-//Call function to compute salary
+				//Conditional statement to compute overtime pay
 
-		grossSalary = computeSalary(hours, hourlyRate, otPay, bonusPay);
-		
-//Input deductions
-	
-		cout<<endl<<"———Enter Deductions (0 if none)———"<<endl;
-		cout<<"Cash Advance: ";
-		cin>>cashAdvance;
-		cout<<"SSS Contribution: ";
-		cin>>sssContri;
-		cout<<"SSS Loan: ";
-		cin>>sssLoan;
-		cout<<"Pag-Ibig Contribution: ";
-		cin>>pagibigContri;
-		cout<<"Pag-Ibig MPL: ";
-		cin>>pagibigLoan;
-		cout<<"PhilHealth: ";
-		cin>>philHealth;
-		cout<<"Others: ";
-		cin>>others;
+                if (hours > 160)
+    		     {
+             	    otPay = (hours - 160) * (hourlyRate * 1.25);
+    		     }
+       	          else 
+      	           {
+                         otPay = 0;
+         	        }        
 
-//Compute total deductions
+				//Select bonus category
 
-		totalDeduction = cashAdvance + sssContri + sssLoan + pagibigContri + pagibigLoan + philHealth + others;
-	
-//Compute netpay
+                 cout<<endl<<"Select a bonus category (1 - Standard, 2 - Premium, 3 - None): ";
+                 cin>>category;
 
-		netPay = grossSalary - totalDeduction;
-		
-//Call function to display payslip summary
+                 switch (category)        
+                 {
+                         case 1:
+                                 bonusPay = 500;
+                                 break;
+                         case 2:
+                                 bonusPay = 1000;
+                                 break;
+                         case 3:
+                                 bonusPay = 0;
+                                 break;
+                         default:
+                                 cout<<"Invalid category. No bonus applied.";
+                                 bonusPay = 0;
+                                 break;
+                 }
 
-		displaySummary(id, name, hours, hourlyRate, grossSalary, otPay, bonusPay, cashAdvance, sssContri, sssLoan, pagibigContri, pagibigLoan, philHealth, others, totalDeduction, netPay);
+				//Call function to compute gross salary
 
-	}
-			
-	return EXIT_SUCCESS;
+                grossSalary = computeSalary(hours, hourlyRate, otPay, bonusPay);
+
+				//Input deductions
+
+                cout<<endl<<"———Enter Deductions (0 if none)———"<<endl;
+                cout<<"Cash Advance: ";
+                cin>>cashAdvance;
+                cout<<"SSS Contribution: ";
+                cin>>sssContri;
+                cout<<"SSS Loan: ";
+                cin>>sssLoan;
+                cout<<"Pag-Ibig Contribution: ";
+                cin>>pagibigContri;
+                cout<<"Pag-Ibig MPL: ";
+                cin>>pagibigLoan;
+                cout<<"PhilHealth: ";
+                cin>>philHealth;
+                cout<<"Others: ";
+                cin>>others;
+
+				//Compute total deductions
+
+                totalDeduction = cashAdvance + sssContri + sssLoan + pagibigContri + pagibigLoan + philHealth + others;
+
+				//Compute net salary
+
+                netPay = grossSalary - totalDeduction;
+
+				//Call function to display payslip summary
+
+                displaySummary(id, name, hours, hourlyRate, grossSalary, otPay, bonusPay, cashAdvance, sssContri, sssLoan, pagibigContri, pagibigLoan, philHealth, others, totalDeduction, netPay);
+
+        }
+
+        return EXIT_SUCCESS;
 }
